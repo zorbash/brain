70f9e7bdd943be77cf10bcf04623e29174380e1d
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2021-10-24 10:56:29 +0100

    Add NervesLivebook.Notes.all/0

--- lib/nerves_livebook/notes.ex
+++ lib/nerves_livebook/notes.ex
@@ -1,5 +1,5 @@
 defmodule NervesLivebook.Notes do
-  @notes_filepath :nerves_livebook
+  @notes :nerves_livebook
                   |> Application.app_dir("priv")
                   |> Path.join("notes/books.json")
                   |> File.read!()
@@ -9,7 +9,9 @@ defmodule NervesLivebook.Notes do
                         do: %{author: author, title: title, text: text}
                   end)
 
+  def all, do: @notes
+
   def random do
-    @notes_filepath |> Enum.random()
+    @notes |> Enum.random()
   end
 end
